2bffae20cf22809041e093322adaca1ca25f3054
2102kh <144224944+2102kh@users.noreply.github.com>
2026-02-25 14:08:53 +0100

    refactor(dashboard): implement reactive state management using RxJS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
+import { Layout } from './shared/layout/layout';
 import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', component: Dashboard },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: Dashboard }
+    ]
+  }
 ];
--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,24 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Subject, switchMap, map, startWith, catchError, of, Observable, tap } from 'rxjs';
+
 import { DashboardDataService, Metric } from '../../services/dashboard-data.service';
 import { MetricCard } from '../../components/metric-card/metric-card';
+
+interface DashboardState {
+  loading: boolean;
+  error: boolean;
+  metrics: Metric[];
+}
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MetricCard],
+  imports: [MetricCard, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
-export class Dashboard implements OnInit {
+export class Dashboard {
 
-  metrics: Metric[] = [];
+  private refreshTrigger$ = new Subject<void>();
 
-  constructor(private dashboardService: DashboardDataService) {}
+  state$: Observable<DashboardState> = this.refreshTrigger$.pipe(
 
-  ngOnInit(): void {
-    this.dashboardService.getMetrics().subscribe(data => {
-      this.metrics = data;
-    });
+    tap(() => console.log('🟣 Trigger emitted')),
+
+    startWith(void 0),
+
+    switchMap(() =>
+      this.dashboardService.getMetrics().pipe(
+
+        tap(() => console.log('🟡 API call started')),
+
+        map((metrics: Metric[]) => ({
+          loading: false,
+          error: false,
+          metrics
+        })),
+
+        tap(state => console.log('🟢 Success state:', state)),
+
+        startWith({
+          loading: true,
+          error: false,
+          metrics: []
+        }),
+
+        tap(state => {
+          if (state.loading) {
+            console.log('🔵 Loading state emitted');
+          }
+        }),
+
+        catchError((error) => {
+          console.log('🔴 Error caught:', error);
+          return of({
+            loading: false,
+            error: true,
+            metrics: []
+          });
+        })
+      )
+    )
+  );
+
+  constructor(private dashboardService: DashboardDataService) {
+    console.log('🧱 Dashboard component created');
+  }
+
+  refresh() {
+    console.log('🔘 Refresh button clicked');
+    this.refreshTrigger$.next();
   }
 
 }
