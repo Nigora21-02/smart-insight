254989d981a2190224907c933592e65c0e4b999a
2102kh <144224944+2102kh@users.noreply.github.com>
2026-02-28 14:44:46 +0100

    feat: add responsive layout and mobile hamburger navigation

--- src/app/shared/layout/layout.ts
+++ src/app/shared/layout/layout.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
 export class Layout {
+  isSidebarOpen = false;
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen = false;
+  }
 }
